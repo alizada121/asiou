b10697a3946167721af3552ba0085131bb6cf188
alizada121 <alizadal@inbox.ru>
2023-01-05 00:33:43 +0400

    son son son

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
@@ -29,6 +31,11 @@ const webBtn=document.getElementById("webBtn")
 const allBtn=document.getElementById("allBtn")
 const designBtn=document.getElementById("designBtn")
 const brandBtn=document.getElementById("brandBtn")
+
+const portfolio1=document.getElementById("portfolio1")
+const portfolio2=document.getElementById("portfolio2")
+const portfolio3=document.getElementById("portfolio3")
+
   function web(){
     webBtn.style.backgroundColor="#007bff"
     webBtn.style.color="white"
@@ -41,6 +48,12 @@ const brandBtn=document.getElementById("brandBtn")
 
     brandBtn.style.backgroundColor="white"
     brandBtn.style.color="black"
+
+    portfolio1.style.display="none"
+    portfolio3.style.display="none"
+    portfolio2.style.display="flex"
+
+
      
   }
 
@@ -57,6 +70,11 @@ const brandBtn=document.getElementById("brandBtn")
 
     brandBtn.style.backgroundColor="white"
     brandBtn.style.color="black"
+
+    portfolio2.style.display="none"
+    portfolio3.style.display="none"
+    portfolio1.style.display="flex"
+
      
   }
 
@@ -72,6 +90,11 @@ const brandBtn=document.getElementById("brandBtn")
 
     brandBtn.style.backgroundColor="white"
     brandBtn.style.color="black"
+
+    portfolio3.style.display="none"
+    portfolio2.style.display="none"
+    portfolio1.style.display="flex"
+
      
   }
 
@@ -87,7 +110,73 @@ const brandBtn=document.getElementById("brandBtn")
 
     webBtn.style.backgroundColor="white"
     webBtn.style.color="black"
+
+    portfolio1.style.display="none"
+    portfolio3.style.display="none"
+    portfolio2.style.display="flex"
+
      
   }
+
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({ease:"none",duration:1})
+
+
+gsap.from(".about", {
+  y: 250,
+  opacity:0,
+  scrollTrigger: {
+    trigger: ".about",
+    start: "top center",
+    end: "top 250px",
+    // scrub: true,
+    // markers: true,
+    id: "scrub"
+  }
+});
+
+
+
+gsap.from(".team", {
+  y: 250,
+  opacity:0,
+  
+  scrollTrigger: {
+    trigger: ".team",
+    start: "top 200px",
+    end: "top 300px",
+    // scrub: true,     
+    markers: true,
+    id: "scrub",
+  
+  }
+});
+
+
+gsap.from(".portfolio", {
+  y: 250,
+  opacity:0,
+  
+  scrollTrigger: {
+    trigger: ".portfolio",
+    start: "top 200px",
+    end: "top 300px",
+    // scrub: true,     
+    markers: true,
+    id: "scrub",
+  
+  }
+});
+
+const about=document.getElementById("nav-more")
+
+// function openAbout(){
+//   about.style.display="flex"
+// }
+
+// function closeAbout(){
+//   about.style.display="none"
+// }
 
 
